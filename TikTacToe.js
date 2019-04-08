1f9bbf8eae3df57b7e5cda800d4467ecf394ab7f
danny <danny151515@naver.com>
2019-04-08 19:35:56 +0900

    Add Js File [TikTacToe.js]

---
+++ TikTacToe.js
@@ -0,0 +1,11 @@
+var 바디 = document.body;
+var 테이블 = document.createElement('table');
+for(var i = 1;i <= 3;i+=1) {
+  var 줄 = document.createElement('tr');
+  for(var j = 1;j <=3;j+=1) {
+    var 칸 = document.createElement('td');
+    줄.appendChild(칸);
+  }
+  테이블.appendChild(줄);
+}
+바디.appendChild(테이블);
